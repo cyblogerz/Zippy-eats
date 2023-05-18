86a756c06deaf6a1bdbf8ec964502ad243fe835b
Pranav Ajay <pranavajay2002@gmail.com>
2023-05-18 14:22:05 +0530

    Updated Message Bubble

---
+++ lib/providers/favourites_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FavouriteMealsNotifier extends StateNotifier{
+
+}
+
+final favouriteMealsProvider = Provider((ref) => );
+//if you have more complex data that should change under certain circums we use stateNotifier 
